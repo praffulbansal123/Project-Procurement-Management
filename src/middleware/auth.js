37267b54c9da9b5bb1a2d7a024dbf593777effcb
praffulbansal123 <prafful.bansal@gmail.com>
2022-07-29 01:52:04 +0530

    Updated auth file and errors handeled

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,17 +3,19 @@ import jwt from "jsonwebtoken";
 
 export const authentication = async function (req, res, next) {
   try {
+
+    if (!req.headers.authorization) throw createError.NotFound("Token not found..please login first");
     const token = req.headers.authorization.split(" ")[1];
-    if (!token) throw createError.NotFound("Token not found..please login first");
 
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET, {
       ignoreExpiration: true,
     });
+    if(!decodedToken) throw createError.Unauthorized()
     if (Date.now() > decodedToken.exp * 1000)
       throw createError.RequestTimeout(" session expired please login again");
     req.decodedToken = decodedToken;
     next();
   } catch (err) {
-    return createError.Unauthorized(err.message)
+    next(err)
   }
 };
